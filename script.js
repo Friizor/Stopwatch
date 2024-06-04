ed1cf10858b2c5dfd0380c0af1084188f3de56fb
friizor <mohamedzaidi768@gmail.com>
2024-06-04 20:20:21 +0100

    setting the neccessar js

---
+++ script.js
@@ -0,0 +1,24 @@
+const watch = document.getElementById('display');
+let timer = null;
+
+let startTime = 0;
+let eloTime = 0;
+
+let status = false;
+
+
+function start() {
+
+}
+
+function stop() {
+
+}
+
+function reset() {
+
+}
+
+function displaying() {
+
+}
